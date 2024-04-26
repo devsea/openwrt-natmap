b8ee7f038221f2598d77d020e2756c09e47c9bed
John Doe <johndoe@example.com>
2024-04-26 21:23:07 +0800

    Revert "password"
    This reverts commit 04831e23007fe98e186ef96c57357f245e66ddf5.

--- luci-app-natmap/htdocs/luci-static/resources/view/natmap.js
+++ luci-app-natmap/htdocs/luci-static/resources/view/natmap.js
@@ -237,10 +237,11 @@ return view.extend({
 
     o = s.taboption(
       "forward",
-      form.Password,
+      form.Value,
       "forward_ikuai_password",
       _("Ikuai Password")
     );
+    o.datatype = "string";
     o.modalonly = true;
     o.depends("forward_mode", "ikuai");
 
@@ -628,7 +629,7 @@ return view.extend({
     o.modalonly = true;
     o.depends("link_mode", "qbittorrent");
 
-    o = s.taboption("link", form.Password, "link_qb_password", _("Password"));
+    o = s.taboption("link", form.Value, "link_qb_password", _("Password"));
     o.datatype = "string";
     o.modalonly = true;
     o.depends("link_mode", "qbittorrent");
@@ -667,7 +668,7 @@ return view.extend({
     o.modalonly = true;
     o.depends("link_mode", "transmission");
 
-    o = s.taboption("link", form.Password, "link_tr_password", _("Password"));
+    o = s.taboption("link", form.Value, "link_tr_password", _("Password"));
     o.datatype = "string";
     o.modalonly = true;
     o.depends("link_mode", "transmission");
