4567705506401bb25ac8a490037fb54209e3ef95
John Doe <johndoe@example.com>
2024-09-05 17:00:51 +0800

    fix

--- luci-app-natmap/root/usr/share/natmap/link.sh
+++ luci-app-natmap/root/usr/share/natmap/link.sh
@@ -24,12 +24,36 @@ else
 fi
 
 # 如果$LINK_MODE非空则执行对应的脚本
-if [ -n "${LINK_MODE}" ]; then
-    link_script="/usr/share/natmap/plugin-link/${LINK_MODE}.sh"
-fi
+case "${LINK_MODE}" in
+"cloudflare_ddns")
+	link_script="/usr/share/natmap/plugin-link/cloudflare_ddns.sh"
+	;;
+"cloudflare_origin_rule")
+	link_script="/usr/share/natmap/plugin-link/cloudflare_origin_rule.sh"
+	;;
+"cloudflare_redirect_rule")
+	link_script="/usr/share/natmap/plugin-link/cloudflare_redirect_rule.sh"
+	;;
+"emby")
+	link_script="/usr/share/natmap/plugin-link/emby.sh"
+	;;
+"qbittorrent")
+	link_script="/usr/share/natmap/plugin-link/qbittorrent.sh"
+	;;
+"transmission")
+	link_script="/usr/share/natmap/plugin-link/transmission.sh"
+	;;
+*)
+	link_script=""
+	;;
+esac
+
+# if [ -n "${LINK_MODE}" ]; then
+#     link_script="/usr/share/natmap/plugin-link/${LINK_MODE}.sh"
+# fi
 
 if [ -n "${link_script}" ]; then
-    echo "$(date +'%Y-%m-%d %H:%M:%S') : $GENERAL_NAT_NAME execute link script" >>/var/log/natmap/natmap.log
-    echo "$(date +'%Y-%m-%d %H:%M:%S') : $GENERAL_NAT_NAME execute link script"
-    bash "${link_script}" "$outter_ip" "$outter_port" "$ip4p" "$inner_port" "$protocol" "$max_retries" "$sleep_time"
+	echo "$(date +'%Y-%m-%d %H:%M:%S') : $GENERAL_NAT_NAME execute link script" >>/var/log/natmap/natmap.log
+	echo "$(date +'%Y-%m-%d %H:%M:%S') : $GENERAL_NAT_NAME execute link script"
+	bash "${link_script}" "$outter_ip" "$outter_port" "$ip4p" "$inner_port" "$protocol" "$max_retries" "$sleep_time"
 fi
